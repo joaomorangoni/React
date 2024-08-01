ab0e7e0b53612b67cd109fc1642cc9f332f603e0
joaomorangoni <joaopedromorangoni@gmail.com>
2024-08-01 11:10:46 -0300

    Primeira Versão do EtecFlix

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+     <h1>ETEC MCM!!</h1>
+      <p>Olá Mundo!</p>
+    </div>
+  );
+}
+
+export default App;
