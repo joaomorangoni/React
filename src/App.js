f706db7a62401b5b242f5594ce09a4a412cefe65
joaomorangoni <joaopedromorangoni@gmail.com>
2024-08-08 10:57:49 -0300

    bungas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <div>
+    <>
       <Header/>
-     <h1>ETEC MCM!!</h1>
-      <p>Olá Mundo!</p>
-    </div>
+      <Banner/>
+      <Container>
+        <h1>ETEC MCM!!</h1>
+        <p>Olá Mundo!</p>
+      </Container>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer (){
+    return(
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por João</h2>
+        </footer>
+    );
+}
+
+export default Footer;
