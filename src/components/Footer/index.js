2947d20488dc62dc2f5988d149219c3c7b950635
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-09-23 08:06:27 -0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> EtecFlix &copy; Desenvolvido por João Pedro Morangoni</h2>
+        </footer>
+    );
+}
+
+export default Footer;
